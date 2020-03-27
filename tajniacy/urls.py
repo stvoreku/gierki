b9b8269048c89d9403607fa944400942f9eb8dbf
Jacek <stvoreku@gmail.com>
2020-03-27 19:25:39 +0100

    pierwszy widok

--- tajniacy/urls.py
+++ tajniacy/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home'),
+    path('<int:pk>/', views.GameView.as_view(), name='game'),
 ]
--- tajniacy/views.py
+++ tajniacy/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
+from .models import Game
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+    def get_context_data(self, **kwargs):
+
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['games_list'] = Game.objects.all()
+
+        return context
+
+class GameView(TemplateView):
+    template_name = 'game.html'
