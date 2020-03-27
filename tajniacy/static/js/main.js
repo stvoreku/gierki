dbd28ed3d2c5b7a063bcf52fa48f49ad0d519350
Jacek <stvoreku@gmail.com>
2020-03-27 19:47:30 +0100

    pierwszy widok

--- tajniacy/static/js/main.js
+++ tajniacy/static/js/main.js
@@ -54,3 +54,23 @@ $(function() {
 
 
 
+function marco() {
+
+$.ajax({
+    url : "", // the endpoint
+    type : "POST", // http method
+    data : {
+
+    }, // data sent with the post request
+
+    success : function(json) {
+	    console.log(json)
+        },
+
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+
+    }
