ac94f44b53b81171d3ae0979b8a26bd2221f75dd
Jacek <stvoreku@gmail.com>
2020-03-27 19:03:29 +0100

    pierwszy widok

--- tajniacy/admin.py
+++ tajniacy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Game)
+admin.site.register(Team)
+admin.site.register(Profile)
+admin.site.register(Card)
+admin.site.register(Word)
