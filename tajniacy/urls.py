046facdc3edc55b9769f841919a4978d631798ec
Jacek <stvoreku@gmail.com>
2020-03-28 16:40:32 +0100

    Gierki tworzenie

--- tajniacy/urls.py
+++ tajniacy/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home'),
     path('<int:pk>/', views.GameView.as_view(), name='game'),
+    path('game/', views.GameUpdate.as_view(), name='gameupdate'),
 ]
