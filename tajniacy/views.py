c311d8ea8293501d0489003c99ae21ccc7698bc5
Jacek <stvoreku@gmail.com>
2020-04-05 18:46:02 +0200

    game update

--- tajniacy/views.py
+++ tajniacy/views.py
@@ -106,7 +106,7 @@ class GameUpdate(View):
                 if game.status == 'active':
                     if Team.objects.filter(game=game, leader=current_user).count() > 0:
                         for card in Cards:
-                            card_list.append({'id': card.id, 'word': card.word.word, 'status': card.status, 'visible': card.visible, 'uncovered_by': card.uncovered_by.username})
+                            card_list.append({'id': card.id, 'word': card.word.word, 'status': card.status, 'visible': card.visible})
                     else:
                         for card in Cards:
                             if card.visible == True:
@@ -116,5 +116,4 @@ class GameUpdate(View):
                 elif game.status == 'new':
                     for card in Cards:
                             card_list.append({'id':card.id, 'word': card.word.word})
-
                 return JsonResponse({'cards': card_list})
